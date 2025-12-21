ee376e697da4191be012a66b88dac0c12dac7482
SalasNorman <salasnorman015@gmail.com>
2025-12-21 18:49:42 +0800

    add my solution

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (text, times) {
+  let newText = "";
+  if (times < 0) {
+    return "ERROR";
+  } else {
+    for (let i = 1; i <= times; i++) {
+      newText += text;
+    }
+    return newText;
+  }
 };
 
 // Do not edit below this line
--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (text) {
+  let newText = "";
+  for (let i = 1; i <= text.length; i++) {
+    newText += text.at(-i);
+  }
+  return newText;
 };
 
 // Do not edit below this line
