1b6b32e3d259db781623109c5f36589dcc1f4201
SalasNorman <salasnorman015@gmail.com>
2025-12-27 11:30:05 +0800

    add my solution

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-    
+const removeFromArray = function () {
+  for (let j = 1; j < arguments.length; j++) {
+    for (let i = 0; i < arguments[0].length; i++) {
+      if (arguments[0][i] === arguments[j]) {
+        arguments[0].splice(i, 1);
+        i--;
+      }
+    }
+  }
+  return arguments[0];
 };
 
 // Do not edit below this line
