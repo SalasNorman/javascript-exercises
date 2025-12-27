59592a52a05d4bb21c1a766ba8d43b0165bb37ab
SalasNorman <salasnorman015@gmail.com>
2025-12-27 14:38:06 +0800

    add my solution

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farhrenheit) {
+  let result = (farhrenheit - 32) / 1.8;
+  return parseFloat(result.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let result = celsius * 1.8 + 32;
+  return parseFloat(result.toFixed(1));
 };
 
 // Do not edit below this line
