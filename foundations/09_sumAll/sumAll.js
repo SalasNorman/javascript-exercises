380d3ac7044e65cd7c7b66db69007e515cb01000
SalasNorman <salasnorman015@gmail.com>
2025-12-27 13:58:24 +0800

    add my solution

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  let total = 0;
+  if (Number.isInteger(a) && a > 0 && Number.isInteger(b) && b > 0) {
+    if (a < b) {
+      for (; a <= b; a++) {
+        total += a;
+      }
+    } else if (b < a) {
+      for (; b <= a; b++) {
+        total += b;
+      }
+    }
+  } else {
+    return "ERROR";
+  }
+  return total;
 };
 
 // Do not edit below this line
