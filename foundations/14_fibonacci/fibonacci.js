65bd8d60633dc56a25f22890796367a70e14ced3
SalasNorman <salasnorman015@gmail.com>
2026-01-12 19:02:03 +0800

    add my solution

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (Math.sign(num) === -1) {
+    return "OOPS";
+  } else {
+    let prev1 = 0;
+    let prev2 = 1;
+    let curr = 0;
+    for (let i = 1; i <= num; i++) {
+      curr = prev1 + prev2;
+      prev2 = prev1;
+      prev1 = curr;
+    }
+    return curr;
+  }
 };
 
 // Do not edit below this line
