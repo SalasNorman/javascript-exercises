4707d0eb069a021680aa8c7519bcc806d4b43736
SalasNorman <salasnorman015@gmail.com>
2026-01-12 18:14:34 +0800

    add my solution

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+  let newText = text.toLowerCase().replace(/[^a-z0-9]/g, "");
+  let compareText = newText.split("").reverse().join("");
+  return compareText === newText ? true : false;
 };
 
 // Do not edit below this line
