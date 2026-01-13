3622a7814a554aba3bce546c0dfde8edbea9dfe0
SalasNorman <salasnorman015@gmail.com>
2026-01-13 17:13:23 +0800

    add my solution

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  const currentYear = new Date().getFullYear();
+  return people.sort(function (a, b) {
+    if (a.yearOfDeath === undefined) {
+      a.yearOfDeath = currentYear;
+    } else if (b.yearOfDeath === undefined) {
+      b.yearOfDeath = currentYear;
+    }
+    const firstPerson = a.yearOfDeath - a.yearOfBirth;
+    const nextPerson = b.yearOfDeath - b.yearOfBirth;
+    return firstPerson > nextPerson ? -1 : 1;
+  })[0];
 };
 
 // Do not edit below this line
